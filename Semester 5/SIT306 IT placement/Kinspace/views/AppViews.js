a7fbd2ebe63b43739661cfb9c0f4b2a022e1a065
Ali Asghar Fakhruddin <aliasgharfakhruddin@Alis-MacBook-Pro-2.local>
2022-05-18 18:16:03 +1000

    final

---
+++ Semester 5/SIT306 IT placement/Kinspace/views/AppViews.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+const exports = {};
+
+exports.Wrapper = class extends React.Component {
+  render() {
+    const {content} = this.props;
+    return (
+      <div className="App">
+        <header className="App-header" id="root">
+          <h2 className='logo'></h2>
+          {content}
+        </header>
+      </div>
+    );
+  }
+}
+
+exports.ConnectAccount = class extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Collectible NFTs for doing good things in DEI in your workplace.</h1>
+        Badges are ALGO NFTs, so you can collect and share them on Twitter, Instagram, or your internal socials.
+
+
+      </div>
+    )
+  }
+}
+
+exports.FundAccount = class extends React.Component {
+  render() {
+    const {bal, standardUnit, defaultFundAmt, parent} = this.props;
+    const amt = (this.state || {}).amt || defaultFundAmt;
+    return (
+      <div>
+        <h2>Fund account</h2>
+        <br />
+        Balance: {bal} {standardUnit}
+        <hr />
+        Would you like to fund your account with additional {standardUnit}?
+        <br />
+        (This only works on certain devnets)
+        <br />
+        <input
+          type='number'
+          placeholder={defaultFundAmt}
+          onChange={(e) => this.setState({amt: e.currentTarget.value})}
+        />
+        <button onClick={() => parent.fundAccount(amt)}>Fund Account</button>
+        <button onClick={() => parent.skipFundAccount()}>Skip</button>
+      </div>
+    );
+  }
+}
+
+exports.DeployerOrAttacher = class extends React.Component {
+  render() {
+    const {parent} = this.props;
+    return (
+      <div>
+        Are you here to collect a badge for completing a module? 
+        <br />
+        <p>
+          <button
+            onClick={() => parent.selectDeployer()}
+          >Yes</button> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <button
+            onClick={() => parent.selectAttacher()}
+          >No</button>
+          <br />
+        </p>
+      </div>
+    );
+  }
+}
+
+export default exports;
---
+++ Semester 5/SIT306 IT placement/Kinspace/views/PlayerViews.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const exports = {};
+
+exports.ViewResults = class extends React.Component {
+  render() {
+    const {outcome} = this.props;
+    console.log(`outcomes are ${outcome[0]}, ${outcome[1]}, ${outcome[2]}`);
+    // BW: We may want to improve the UI here 
+       
+    return (
+      <div>
+        Alice's current results are:
+        <br />{String(outcome[0])}, {String(outcome[1])}, {String(outcome[2])}
+      </div>
+    );
+  }
+}
+
+exports.UpdateResults = class extends React.Component {
+  render() {
+    return (
+      <div>
+        Issuing the module badge...
+      </div>
+    );
+  }
+}
+
+exports.Timeout = class extends React.Component {
+  render() {
+    return (
+      <div>
+        There's been a timeout.
+      </div>
+    );
+  }
+}
+
+export default exports;
